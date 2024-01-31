cebf163b843942336a1c0e00e9ceb2dd2f04ace9
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-01-31 11:39:00 +0900

    Change hello count version 4
    Change file app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,33 @@
 import { ref, onMounted } from 'vue'
 
 // reactive state
-const count = ref(0)
-
+const count = ref({
+	cnt: 0,
+	name: "kim",
+})
 
 // function that mutate state and trigger updates
 function increment() {
-  count.value++
+  count.value.cnt = count.value.cnt + 1
+  count.value.name = count.value.name.split('').reverse().join('')
 }
 
 // lifecycle hooks
-onMounted(() => console.log('The initial count is ${count.value}.'))
+onMounted(() => console.log(`The initial count is ${count.value}.`))
 </script>
 
 <template>
-  <button @click="increment">Count is : {{ count > 10 ? "크다": "작다" }}</button>
+  <button @click="increment" class="custom-button">
+    Count is : {{ count.cnt }} {{ count.name }}
+  </button>
 </template>
 
 <style scoped>
-button {
+button.custom-button {
   font-weight: bold;
+  font-size: 16px; /* 크기 조정 */
+  color: white; /* 글자색 */
+  background-color: #4caf50; /* 배경색 */
+  padding: 10px 20px; /* 내부 여백 */
 }
 </style>
-
