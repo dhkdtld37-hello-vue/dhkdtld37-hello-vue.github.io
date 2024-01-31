3512b6effc58ab6d55e38bb07aa650d90dba9f53
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-01-31 10:57:32 +0900

    hello count version 2
    change file app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
-import { ref } from 'vue'
-const count = ref(0)
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(1)
+const count = 0
+
+// function that mutate state and trigger updates
+function increment() {
+  count = count + 1
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log('The initial count is ${count.value}.')
+})
 </script>
 
 <template>
-	<button @click="count++">Count is: {{ count }}</button>
+	<button @click="increment">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
