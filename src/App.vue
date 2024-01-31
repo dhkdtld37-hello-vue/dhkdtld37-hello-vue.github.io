428c648c15bb55fda9a37ac9cb4cdad0f321d51a
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-01-31 12:51:49 +0900

    Change vue file
    Change vue file

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,25 @@
 import { ref } from 'vue'
 
 const awesome = ref(true)
+
+function toggleAwesome(v) {
+  awesome.value = !v
+}
+
+function isAwesome() {
+  return awesome.value
+}
+
+
 </script>
 
 <template>
-  <button @click="awesome = !awesome">toggle</button>
+  <button @click="toggleAwesome(awesome)">toggle</button>
 
-  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-if="isAwesome()">Vue is awesome!</h1>
   <h1 v-else>Oh no 😂 </h1>
-  <h4 v-if="awesome">TURE</h4>
-  <h4 v-show="awesome">TRUE</h4>
-</template>
 
+  <h4 v-if="isAwesome">v-if TURE</h4>
+  <h4 v-show="isAwesome()">v-show TURE</h4>
+</template>
 
