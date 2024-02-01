65574818038f96ff4c37f8692b88f54634e62b15
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-01 16:12:13 +0900

    수정중

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 import { ref } from 'vue'
-
 import team from "@/assets/2018WorldCup.json"
 
-const teams = ref(team)
+const teams = team
+const teamK = ref(true)
+
+const filterK = (item) => {
+  return item && item.name && item.name.toLowerCase().includes('k');
+};
+
+const 
+
 
 </script>
 
@@ -11,11 +18,14 @@ const teams = ref(team)
   <div>
 	<h2>2018 WORLD CUP TEAM</h2>
 	<ul>
-	  <li v-for="item in teams" v-if="item && item.name  && item.name.toLowerCase().includes('j')" >
+	  <li v-for="item in teams">
 		{{ item.name }} - {{ item.id }}
 		<img :src="item.flag" alt="Team Flag" style="max-width: 100px; max-height: 100px;">
 	  </li>
 	</ul>
+
+	<ul>
+	  <li 
 
   </div>
 </template>
