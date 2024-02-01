05de73eddd01781ffdb259d82939f657b6a67daa
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-01 17:53:53 +0900

    Change App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 import Child from './components/Child.vue'
+
+const pocketMoney = ref('부모 컴포넌트로부터 <🎟>을 전달받았어요!')
 </script>
 
 <template>
-  <Child />
+  <Child :pocketMoney="pocketMoney" />
 </template>
