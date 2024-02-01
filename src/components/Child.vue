ddf8704e8b6422bc1cc008fb9b97ce5b8f7719e7
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-01 17:43:45 +0900

    Change Child.vue

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,3 +1,9 @@
+<script setup>
+const props = defineProps({
+	pocketMoney: String
+})
+</script>
+
 <template>
-	<h2>자식 컴포넌트입니다!</h2>
+	<h2>{{ pocketMoney || '용돈이 아직 전달되지 않았습니다!' }}</h2>
 </template>
