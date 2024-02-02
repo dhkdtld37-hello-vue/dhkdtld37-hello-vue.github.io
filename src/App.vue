e42c37fec7c82758d8c7573dcfbc93dbc3147434
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-02 09:31:02 +0900

    k가 들어간 팀 이름

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,13 @@ import { ref } from 'vue'
 import team from "@/assets/2018WorldCup.json"
 
 const teams = team
-const teamK = ref(true)
-
-const filterK = (item) => {
-  return item && item.name && item.name.toLowerCase().includes('k');
-};
-
-const itemK = filterK()
 
 </script>
 
 <template>
   <div>
-	<h2>2018 WORLD CUP TEAM</h2>
-	<ul>
-	  <li v-for="item in teams">
-		{{ item.name }} - {{ item.id }}
-		<img :src="item.flag" alt="Team Flag" style="max-width: 100px; max-height: 100px;">
-	  </li>
-	</ul>
-
-	<ul v-if="teamK">
-	  <li v-for="item in itemK"> 
+	<ul v-for="item in teams">
+	  <li v-if="item.name.toLowerCase().includes('k')"> 
 		 {{ item.name }} - {{ item.id }}
                 <img :src="item.flag" alt="Team Flag" style="max-width: 100px; max-height: 100px;">
           </li>
