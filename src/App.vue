9f4d1b9409a7a4664446035300ceb527b9f99424
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-02 11:49:56 +0900

    0.3.8 / use API
    Change file App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const show = ref(true)
-const list = ref([1, 2, 3])
+const todoId = ref(1); const todoData = ref(null)
+
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch (
+	`https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+}
+fetchData()
+
+watch(todoId, fetchData);
+
+fetchData();
 
 </script>
 
 <template>
-  <button @click="show = !show">List 렌더링 ON / OFF </button>
-  <button @click="list.push(list.length + 1)">List 추가 </button>
-  <button @click="list.pop()">List 제거 </button>
-  <button @click="list.reverse()">List 뒤집기 </button>
-
-  <ul v-if="show">
-    <li v-for="item of list">{{ item }}</li>
-  </ul>
-  <p v-else-if="list.length">List in not empty, but hidden.</p>
-  <p v-else>List is empty.</p>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++" :disabled="!todoData">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
