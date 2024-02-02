09bf2c2094bb72d8b89accde7b89b9d76e46d85a
dhkdtld37 <dhkdtld37@DESKTOP-GGN85KQ.>
2024-02-02 11:09:56 +0900

    0.3.7 인라인 버전으로 변경
    함수 버전 = App.vue.MorningHamsu
    인라인 버전 = App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,13 @@ import { ref } from 'vue'
 const show = ref(true)
 const list = ref([1, 2, 3])
 
-const ButtonL = () => {
-  show.value = !show.value
-}
-
-const AddL = () => {
- list.value.push(list.value.length + 1)
-}
-
-const RemoveL = () => {
-  list.value.pop()
-}
-
-const ReverseL = () => {
-  list.value.reverse()
-}
-
 </script>
 
 <template>
-  <button @click="ButtonL">List 렌더링 ON / OFF </button>
-  <button @click="AddL">List 추가 </button>
-  <button @click="RemoveL">List 제거 </button>
-  <button @click="ReverseL">List 뒤집기 </button>
+  <button @click="show = !show">List 렌더링 ON / OFF </button>
+  <button @click="list.push(list.length + 1)">List 추가 </button>
+  <button @click="list.pop()">List 제거 </button>
+  <button @click="list.reverse()">List 뒤집기 </button>
 
   <ul v-if="show">
     <li v-for="item of list">{{ item }}</li>
@@ -34,4 +18,3 @@ const ReverseL = () => {
   <p v-else-if="list.length">List in not empty, but hidden.</p>
   <p v-else>List is empty.</p>
 </template>
-
